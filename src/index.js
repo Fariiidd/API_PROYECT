9b75a6d342aca9a121db4f5f5a70a87fe5fdefa8
Farid <faridurielrodriguez@gmail.com>
2021-12-15 15:04:30 -0300

    Remove ignored files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config({path: "dotenv.env"})
-import app from './app'
-
-const port = process.env.PORT 
-
-app.listen(port, (req, res) => {
-    console.log(`Listening on port:`, port)
-})
